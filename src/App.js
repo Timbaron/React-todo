020c30a301ba6d3e403277b3ae55bf1fb890b220
Akiode Timothy <akiodetimothy2017@gmail.com>
2021-05-14 23:42:52 +0100

    Create Input,button and display screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+// const [list, setList] = useState([]);
+
+function addTodo(){
+  alert('Changed!!!');
+}
+
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo">
+        <form>
+          <div className="input">
+            <input type="text" id="todo" required autoFocus/>
+            <button onChange={addTodo}>Add</button>
+          </div>
+        </form>
+        <div className="display">
+          
+        </div>
+      </div>
     </div>
   );
 }
